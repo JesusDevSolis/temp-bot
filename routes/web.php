d7c74a08b68b47e5107edde0709067a2000f565d
Jesus Solis Martinez <jesussolis.dev.23@gmail.com>
2025-10-05 20:14:25 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BitrixMarketController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Página de configuración post-instalación del Market
+Route::get('/setup', [BitrixMarketController::class, 'showSetupPage']);
+Route::post('/setup', [BitrixMarketController::class, 'saveSetupConfig']);
+
+
+Route::get('/bitrix/setup-help', function () {
+    return view('setup-help');
 });
